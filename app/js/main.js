688693f24fa243a99a836018f1d226d7756c666b
Efrem4ik <efrem4ik12@mail.ru>
2019-10-01 17:04:49 +0300

    added homepage

--- app/js/main.js
+++ app/js/main.js
@@ -1,31 +1,31 @@
-$(function(){
+$(function () {
 
   // $('.menu__btn').on('click', function(){
   //   $('.menu__list').slideToggle();
   // }); Для бургера!
 
-  //   $('.class').slick({
-  //   dots: true,
-  //   arrows: false,
-  //   slidesToShow: 4,
-  //   slidesToScroll: 4,
-  // }); Обычный одиночный слайдер!
+  $('.header__slider').slick({
+    dots: false,
+    arrows: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  });
 
-	//  $('.slider-for').slick({
-	//   slidesToShow: 1,
-	//   slidesToScroll: 1,
-	//   arrows: false,
-	//   fade: true,
-	//   asNavFor: '.slider-nav'
-	// });
-	// $('.slider-nav').slick({
-	//   slidesToShow: 3,
-	//   slidesToScroll: 1,
-	//   asNavFor: '.slider-for',
-	//   dots: true,
-	//   centerMode: true,
-	//   focusOnSelect: true
-	// }); Двойной слайдер!
+  //  $('.slider-for').slick({
+  //   slidesToShow: 1,
+  //   slidesToScroll: 1,
+  //   arrows: false,
+  //   fade: true,
+  //   asNavFor: '.slider-nav'
+  // });
+  // $('.slider-nav').slick({
+  //   slidesToShow: 3,
+  //   slidesToScroll: 1,
+  //   asNavFor: '.slider-for',
+  //   dots: true,
+  //   centerMode: true,
+  //   focusOnSelect: true
+  // }); Двойной слайдер!
 
 
   // $('.wrapper .tab').on('click', function(event) {
